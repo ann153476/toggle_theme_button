926506ebe7375528de5cdcf27f7dd109b68168e8
Anka <103377387+ann153476@users.noreply.github.com>
2023-01-26 20:19:51 +0200

    new

--- src/js/index.js
+++ src/js/index.js
@@ -2,13 +2,18 @@
 
 const toggle = document.querySelector('.toggle');
 const body = document.querySelector('body');
-const svgTeme = document.querySelectorAll('.svg__teme');
-console.log(svgTeme);
+const svgL = document.querySelector('.svg__l');
+const svgD = document.querySelector('.svg__d');
+
+
 function colorTeme(){
     body.classList.toggle('light');
     body.classList.toggle('dark');
-    svgTeme.classList.toggle('light__svg');
-    svgTeme.classList.toggle('dark__svg');
+    svgL.classList.toggle('light__svg');
+    svgL.classList.toggle('dark__svg');
+    svgD.classList.toggle('light__svg');
+    svgD.classList.toggle('dark__svg');
+
 }
 
 toggle.addEventListener('click', colorTeme);
